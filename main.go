4aee5624742ee937e5b94cf7005aca93e6fde90b
kohta  <kohtakochi@192.168.0.21>
2022-10-25 18:37:29 +0900

    add book structure

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -11,8 +18,21 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1 style= 'color: steelblue'>Hello</h1>"))
 }
 
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	book := Book{
+		Title:  "The Funslinger",
+		Author: "Stephen King",
+		Pages:  304,
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
 func main() {
 	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/book", GetBook)
 
 	log.Fatal(http.ListenAndServe(":5100", nil))
 }
